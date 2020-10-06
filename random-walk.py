ef94b8a61f068e365fe542ea2c3074b6c1240ba7
SeanPhilippi <sean.philippi@gmail.com>
2020-10-05 19:26:53 -0500

    making a graph.png for one-dimensional random walk

--- random-walk.py
+++ random-walk.py
@@ -4,7 +4,8 @@
 # y starts at 0, x increases or decreases by 1, chosen randomly
 # assign # of steps to take, and plot a visual line graph connecting all of the points
 
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 from random import randint
 
 coords = []
@@ -13,8 +14,21 @@ steps = 200
 
 # step 0 - 199
 for step in range(steps):
-    print('rand int', randint(-1, 1))
+    # print('rand int', randint(-1, 1))
     y = y + randint(-1, 1)
-    print('step, y', f'{step}, {y}')
+    # print('step, y', f'{step}, {y}')
     coords.append([step, y])
-print('coords', coords)
+# print('coords', coords)
+
+xs = []
+ys = []
+
+for [x, y] in coords:
+    xs.append(x)    
+    ys.append(y)    
+
+# fig, axs = plt.subplots(2, 2)
+plt.plot(xs, ys)
+# plt.show()
+plt.savefig("mygraph.png")
+
