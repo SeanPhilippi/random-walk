3cfe1aa4caf658006fea14bb08b00dc9dd81c27c
SeanPhilippi <sean.philippi@gmail.com>
2020-10-20 19:02:15 -0500

    random-walk using a seed

--- randrange.py
+++ randrange.py
@@ -1,9 +1,23 @@
-from random import randrange, seed
+from random import random, seed
 from matplotlib import pyplot
 
+# random number generator with a start point of 1
+# allows for consistent set of random numbers
 seed(1)
-series = [randrange(10) for i in range(10)]
-pyplot.plot(series)
-print('series', series)
-print('series 2', series)
+# create a list
+random_walk = list()
+# if random number between 0 and 1 is less than .5, append -1,
+# else append 1
+random_walk.append(-1 if random() < .5 else 1)
 
+for i in range(50):
+    # randomly make movement -1 or 1
+    movement = -1 if random() < .5 else 1
+    # value to append to list for plot is the existing number +
+    # a random 1 or -1
+    value = random_walk[i] + movement
+    # append the new value to the random_walk list
+    random_walk.append(value)
+# plot the list of coordinates
+pyplot.plot(random_walk)
+pyplot.show()
